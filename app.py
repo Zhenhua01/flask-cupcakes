0f7c0064c5b631711176bc2468763f6aa90ccf8c
Edmond Soun <edmond.soun@gmail.com>
2022-08-08 12:10:52 -0700

    initial model and routes through step two

--- app.py
+++ app.py
@@ -1,1 +1,56 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, request, jsonify
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///cupcakes'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+connect_db(app)
+
+@app.get('/api/cupcakes')
+def list_all_cupcakes():
+    """Return JSON {'cupcakes': [{id, flavor, size, rating, image}, ...]}"""
+    
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+@app.get("/api/cupcakes/<cupcake_id>")
+def list_single_dessert(cupcake_id):
+    """Return JSON {'cupcakes': {id, flavor, size, rating, image}}"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = cupcake.serialize()
+
+    return jsonify(cupcake=serialized)
+
+@app.post('/api/cupcakes')
+def create_dessert():
+    """Create cake from form data & return it.
+
+    Returns JSON {'cupcakes': {id, flavor, size, rating, image}}
+    """
+
+    flavor = request.json["flavor"]
+    size = request.json["size"]
+    rating = request.json["rating"]
+    image = request.json["image"]
+    
+    image = image if image else None
+
+    new_cupcake = Cupcake(flavor=flavor, 
+                          size=size, 
+                          rating=rating, 
+                          image=image)
+
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    # Return w/status code 201 --- return tuple (json, status)
+    return (jsonify(cupcake=serialized), 201)
