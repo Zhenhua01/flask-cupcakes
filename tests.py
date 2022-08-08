f7bae4105e7ef5cfaea35482c81b96fbb08316a7
Edmond Soun <edmond.soun@gmail.com>
2022-08-08 14:39:11 -0700

    tests for patch and delete

--- tests.py
+++ tests.py
@@ -27,6 +27,13 @@ CUPCAKE_DATA_2 = {
     "image": "http://test.com/cupcake2.jpg"
 }
 
+CUPCAKE_DATA_PATCH = {
+	"flavor": "strawberry",
+	"size": "huge"
+}
+
+
+
 
 class CupcakeViewsTestCase(TestCase):
     """Tests for views of API."""
@@ -108,3 +115,41 @@ class CupcakeViewsTestCase(TestCase):
             })
 
             self.assertEqual(Cupcake.query.count(), 2)
+            
+    def test_patch_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.patch(url, json=CUPCAKE_DATA_PATCH)
+            
+            self.assertEqual(resp.status_code, 200)
+
+            data = resp.json
+            
+            self.assertEqual(data, {
+                "cupcake": {
+                    "id": self.cupcake.id,
+                    "flavor": "strawberry",
+                    "size": "huge",
+                    "rating": 5,
+                    "image": "http://test.com/cupcake.jpg"
+                }
+            })
+            
+        
+    def test_delete_cupcake(self):
+        with app.test_client() as client:
+            url = f"/api/cupcakes/{self.cupcake.id}"
+            resp = client.delete(url)
+            
+            self.assertEqual(resp.status_code, 200)
+            
+            data = resp.json
+
+            self.assertEqual(data, {
+                "deleted": f"{self.cupcake.id}"
+            })
+            
+            # self.assertEqual(Cupcake.query.count(), 1)
+            
+
+        
